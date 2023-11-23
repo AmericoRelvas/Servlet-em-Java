545ea6641da917eb2441135c18dd3c839d52763a
Américo Relvas <112661393+AmericoRelvas@users.noreply.github.com>
2023-11-23 17:13:56 -0300

    Add files via upload
    O sistema agora permite que o usuário possa digitar os dados dos alunos na tela e adicionar quantos alunos forem necessários em uma estrutura de dados em memória, e ainda é possível acrescentar uma terceira listagem, na qual seja possível filtrar de acordo com parte do nome do aluno digitado em uma caixa de texto pelo usuário na tela.

--- sp1-ListaAlunos/src/main/java/br/com/servlet/AlunoController.java
+++ sp1-ListaAlunos/src/main/java/br/com/servlet/AlunoController.java
@@ -7,74 +7,118 @@ import java.util.Comparator;
 
 public class AlunoController {
 	
-	public AlunoModel objAlunoModel = new AlunoModel();
-	public List<AlunoModel> listaAluno = new ArrayList<AlunoModel>();
-	
-	public void AdicionarAlunoMock() {
+	// Instância da classe de modelo AlunoModel
+		public AlunoModel objAlunoModel = new AlunoModel();
+		
+		// Lista de alunos não ordenada
+		public List<AlunoModel> listaAluno = new ArrayList<AlunoModel>();
+		
+		// Lista de alunos ordenada
+		public List<AlunoModel> listaAlunoOrdenada = new ArrayList<AlunoModel>();
+
+	    // Método para adicionar alunos fictícios (mock) à lista
+		public void AdicionarAlunoMock() {
+		// Aluno 1
 		 objAlunoModel.Nome = "José";
 		 objAlunoModel.Registro = "RA 1";
 		 objAlunoModel.NomeCurso = "Análise e Desenvolvimento de Sistemas";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 2
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Maria";
 		 objAlunoModel.Registro = "RA 2";
 		 objAlunoModel.NomeCurso = "Análise e Desenvolvimento de Sistemas";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 3
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "João";
 		 objAlunoModel.Registro = "RA 3";
 		 objAlunoModel.NomeCurso = "Análise e Desenvolvimento de Sistemas";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 4
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Luciana";
 		 objAlunoModel.Registro = "RA 4";
 		 objAlunoModel.NomeCurso = "Ciência da Computação";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 5
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Pedro";
 		 objAlunoModel.Registro = "RA 5";
 		 objAlunoModel.NomeCurso = "Ciência da Computação";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 6
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Juliana";
 		 objAlunoModel.Registro = "RA 6";
 		 objAlunoModel.NomeCurso = "Ciência da Computação";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 7
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Denise";
 		 objAlunoModel.Registro = "RA 7";
 		 objAlunoModel.NomeCurso = "Ciência da Computação";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 8
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Luiz";
 		 objAlunoModel.Registro = "RA 8";
 		 objAlunoModel.NomeCurso = "Sistemas de Informação";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 9
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Olga";
 		 objAlunoModel.Registro = "RA 9";
 		 objAlunoModel.NomeCurso = "Sistemas de Informação";
 		 listaAluno.add(objAlunoModel);
-		 
+		 listaAlunoOrdenada.add(objAlunoModel);
+		// Aluno 10
 		 objAlunoModel = new AlunoModel();
 		 objAlunoModel.Nome = "Manoel";
 		 objAlunoModel.Registro = "RA 10";
 		 objAlunoModel.NomeCurso = "Sistemas de Informação";
 		 listaAluno.add(objAlunoModel);
+		 listaAlunoOrdenada.add(objAlunoModel);
 	}
 	
-	public void OrdenarListarAluno() {
-		 Collections.sort(listaAluno);
-	} 
-}
-		 	
-
-
+		// Método para ordenar a lista de alunos
+		public void OrdenarListarAluno() {
+			 // Utiliza o método sort da classe Collections, que usa a implementação compareTo de AlunoModel
+			 Collections.sort(listaAlunoOrdenada);
+		} 
+		
+	    // Método para adicionar um novo aluno à lista
+		public void AdicionarAluno(String nomeAluno, String registro, String nomeCurso) {
+			if(nomeAluno!=null && registro!=null && nomeCurso!=null) {
+				objAlunoModel = new AlunoModel();
+				objAlunoModel.Nome = nomeAluno;
+				objAlunoModel.Registro = registro;
+				objAlunoModel.NomeCurso = nomeCurso;
+				listaAluno.add(objAlunoModel);
+				listaAlunoOrdenada.add(objAlunoModel);
+			}
+		}
+				
+	    // Método para filtrar alunos por nome
+		public List<AlunoModel> FiltrarAlunoPorNome(String nomeAlunoFiltro) {
+			if(nomeAlunoFiltro!=null) {
+				List<AlunoModel> listaAlunoFiltro = new ArrayList<AlunoModel>();
+				for(AlunoModel aluno: listaAluno) {
+					if(aluno.Nome.contains(nomeAlunoFiltro))
+						listaAlunoFiltro.add(aluno);
+				}
+				return listaAlunoFiltro;
+			}
+			else {
+				return listaAluno;
+			}
+		}
+	}
--- sp1-ListaAlunos/src/main/java/br/com/servlet/ListaAlunos.java
+++ sp1-ListaAlunos/src/main/java/br/com/servlet/ListaAlunos.java
@@ -2,6 +2,9 @@ package br.com.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,17 +13,69 @@ import jakarta.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class ListaAlunos extends HttpServlet {
 	
-	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter saidaHtml = response.getWriter();
+	// Lista de alunos filtrados
+		List<AlunoModel> listaAlunoFiltro = new ArrayList<AlunoModel>();
+		
+	    // Instância da classe de controle AlunoController
 		AlunoController objAluno = new AlunoController();
 		
-		objAluno.AdicionarAlunoMock();
+		@Override
+		protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			PrintWriter saidaHtml = response.getWriter();
+			
+	        // Verifica se a lista de alunos está vazia e adiciona alunos fictícios (mock) se necessário
+			if(objAluno.listaAluno.size()==0) {
+				 objAluno.AdicionarAlunoMock();
+			}
+			
+	        // Obtém parâmetros do formulário
+			String nome = request.getParameter("nomeAluno");
+			String registro = request.getParameter("registro");
+			String nomeCurso = request.getParameter("nomeCurso");
+			
+	        // Adiciona um novo aluno se os parâmetros não forem nulos
+			if(nome!=null && registro!=null && nomeCurso!=null) {
+				objAluno.AdicionarAluno(nome, registro, nomeCurso);
+			}
+			
+	        // Ordena e lista os alunos
+			objAluno.OrdenarListarAluno();
+			
+			String nomeAlunoFiltro = request.getParameter("nomeAlunoFiltro");
+
+	        // Filtra os alunos pelo nome, se houver um nome de filtro
+			if(nomeAlunoFiltro!="") {
+				 objAluno.FiltrarAlunoPorNome(nomeAlunoFiltro);
+			}
 		
 		saidaHtml.println("<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"pt-br\" xml:lang=\"pt-br\">");
 		saidaHtml.println("<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>");
 		saidaHtml.println("<body>");
-		saidaHtml.println("Situação Problema 1 - Lista de alunos <br>");
+		saidaHtml.println("Lista de alunos <br>");
+		saidaHtml.println("<table>");
+		saidaHtml.println("<tr>");
+		saidaHtml.println("<td>");
+		saidaHtml.println("<div>");
+		saidaHtml.println("<b>Inserir Alunos</b>");
+		saidaHtml.println("<form action=’listaalunos’>");
+		saidaHtml.println("Nome do aluno: <input type='text' name='nomeAluno'><br>");
+		saidaHtml.println("Registro do aluno: <input type='text' name='registr'><br>");
+		saidaHtml.println("Nome do Curso: <input type='text' name='nomeCurso'><br>");
+		saidaHtml.println("<input type='submit' value='Adicionar'>");
+		saidaHtml.println("</form>");
+		saidaHtml.println("</div>");
+		saidaHtml.println("</td>");
+		saidaHtml.println("<td>");
+		saidaHtml.println("<div> <br>");
+		saidaHtml.println("<b>Filtrar Alunos</b>");
+		saidaHtml.println("<form action='listaalunos'>");
+		saidaHtml.println("Digite parte do nome aluno: <input type='text' name='nomeAlunoFiltro'><br>");
+		saidaHtml.println("<input type='submit' value='Filtrar'>");
+		saidaHtml.println("</form>");
+		saidaHtml.println("</div>");
+		saidaHtml.println("</td>");
+		saidaHtml.println("<tr>");
+		saidaHtml.println("</table>");
 		saidaHtml.println("<b>Ordenação Original</b>");
 		saidaHtml.println("<table>");
 		for(AlunoModel aluno: objAluno.listaAluno) {
@@ -32,10 +87,8 @@ public class ListaAlunos extends HttpServlet {
 		}
 		saidaHtml.println("</table>");
 		saidaHtml.println("<b>Ordem alfabética</b>");
-		
-		objAluno.OrdenarListarAluno();
 		saidaHtml.println("<table>");
-		for(AlunoModel aluno: objAluno.listaAluno) {
+		for(AlunoModel aluno: objAluno.listaAlunoOrdenada) {
 			saidaHtml.println("<tr>");
 			saidaHtml.println("<td>" + aluno.Nome + "</td>");
 			saidaHtml.println("<td>" + aluno.Registro + "</td>");
@@ -43,8 +96,20 @@ public class ListaAlunos extends HttpServlet {
 			saidaHtml.println("</tr>");
 		}
 		saidaHtml.println("</table>");
+		saidaHtml.println("<b>Filtro por Nome</b>");
+		saidaHtml.println("<table>");
+		if(nomeAlunoFiltro!="") {
+			listaAlunoFiltro = objAluno.FiltrarAlunoPorNome(nomeAlunoFiltro);
+			for(AlunoModel aluno: listaAlunoFiltro) {
+				 saidaHtml.println("<tr>");
+				 saidaHtml.println("<td>" + aluno.Nome + "</td>");
+				 saidaHtml.println("<td>" + aluno.Registro + "</td>");
+				 saidaHtml.println("<td>" + aluno.NomeCurso + "</td>");
+				 saidaHtml.println("</tr>");
+			}
+		}
+		saidaHtml.println("</table>");
 		saidaHtml.println("</body>");
 		saidaHtml.println("</html>");
 	}
-}
-
+}	
